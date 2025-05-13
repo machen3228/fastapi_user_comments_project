d3e115bf4771bdc67d223e4096e4aae72ea3c642
Valerii Marchenko <machen3228@gmail.com>
2025-05-13 21:58:02 +0300

    PEP8 refactoring

--- app/api/endpoints/user.py
+++ app/api/endpoints/user.py
@@ -18,8 +18,8 @@ router = APIRouter()
     summary='Регистрация пользователя'
 )
 async def create_new_user(
-    session: AsyncSession = Depends(get_async_session),
-    user_in: UserCreate = Depends(UserCreate.as_form)
+        session: AsyncSession = Depends(get_async_session),
+        user_in: UserCreate = Depends(UserCreate.as_form)
 ):
     await validate_user_before_create(user_in, session)
     new_user = await create_user(user_in, session)
@@ -32,21 +32,21 @@ async def create_new_user(
             summary='Получение информации о текущем пользователе',
             )
 async def read_user_me(
-    current_user: AuthUser = Depends(get_current_auth_user)
+        current_user: AuthUser = Depends(get_current_auth_user)
 ):
     return current_user
 
 
 @router.patch(
-        '/{user_id}/update',
-        response_model=UserOut,
-        summary='Редактирование пользователя',
+    '/{user_id}/update',
+    response_model=UserOut,
+    summary='Редактирование пользователя',
 )
 async def update_existed_user(
-    user_id: int = Path(description='id пользователя'),
-    user_update: UserUpdate = Depends(UserUpdate.as_form),
-    session: AsyncSession = Depends(get_async_session),
-    current_user: AuthUser = Depends(get_current_auth_user)
+        user_id: int = Path(description='id пользователя'),
+        user_update: UserUpdate = Depends(UserUpdate.as_form),
+        session: AsyncSession = Depends(get_async_session),
+        current_user: AuthUser = Depends(get_current_auth_user)
 ):
     result = await update_user(
         user_id,
