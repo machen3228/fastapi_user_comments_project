75aaf696dcdb5a76a1b178576fccd66100dc1119
Valerii Marchenko <machen3228@gmail.com>
2025-05-14 23:36:25 +0300

    small refactoring

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 from app.api.routers import main_router
 from app.core.config import settings
@@ -8,3 +9,6 @@ app = FastAPI(
     description=settings.description)
 
 app.include_router(main_router)
+
+if __name__ == '__main__':
+    uvicorn.run("app.main:app ", reload=True)
